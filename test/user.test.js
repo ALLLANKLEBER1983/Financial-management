8bdf61ab277ee2408fe10d37e397cbc1a69dde8d
ALLANKLEBER1983 <allankleber1983@gmail.com>
2022-05-16 22:52:49 -0300

    adição do teste para não permitir a entrada de emails já existentes

--- test/user.test.js
+++ test/user.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 
 const app = require('../src/app');
 
+const mail = `${Date.now()}@mail.com`
+
 test('Deve listar todos os usuários', () => {
     return request(app).get('/users')
     .then(res => {
@@ -12,7 +14,6 @@ test('Deve listar todos os usuários', () => {
 })
 
 test('Deve inserir usuário com sucesso', () =>{
-    const mail = `${Date.now()}@mail.com`
     return request(app).post('/users')
     .send({name: 'Walter Mitty', mail,passwd:'123456'})
     .then((res) => {
@@ -49,4 +50,13 @@ test('Não deve inserir usuário sem senha', (done) => {
         done();
     }).catch(err => done.fail(err));
 
+})
+
+test('Não deve inserir usuário com email existente', () => {
+    return request(app).post('/users')
+    .send({name: 'Walter Mitty', mail,passwd:'123456'})
+    .then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe('Já existe um usuário com esse email');
+    })
 })
