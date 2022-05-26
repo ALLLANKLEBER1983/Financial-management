23d70d44ec63abd48a801c5d1b5de53e58ec533c
ALLANKLEBER1983 <allankleber1983@gmail.com>
2022-05-25 22:36:53 -0300

    melhoria no tratamento do middleware

--- src/app.js
+++ src/app.js
@@ -2,12 +2,10 @@ const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
 const knexFile = require('../knexfile');
-const knexLogger = require('knex-logger');
 const { query } = require('express');
 
 app.db = knex(knexFile.test);
 
-app.use(knexLogger(app.db));
 
 consign({ cwd: 'src',verbose: false })
     .include('./config/middleware.js')
@@ -20,11 +18,11 @@ app.get('/' ,(req,res) => {
     res.status(200).send();
 });
 
-app.db.on('query',(query) => {
+/*app.db.on('query',(query) => {
     console.log({sql: query.sql, bindings: query.bindings ? query.bindings.join(','):''})
 }).on('query-response',(response) => {
     console.log(response);
-}).on('error',error => console.log(error));
+}).on('error',error => console.log(error));*/
 
 
 module.exports = app;
--- src/config/middleware.js
+++ src/config/middleware.js
@@ -1,5 +1,8 @@
-const bodyParcer = require('body-parser')
+const knexLogger = require('knex-logger');
+const bodyParcer = require('body-parser');
+
 module.exports = (app) =>{
     app.use(bodyParcer.json())
+    app.use(knexLogger(app.db));
 
 }
